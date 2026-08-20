384c70e4fe22cdb7af998ed60be91c7db7c2c646
Richard Silva <richarda50x@gmail.com>
2026-08-19 23:30:48 -0300

    Streams no Node.js - Entendendo Streams no Node.js

---
+++ aulas-node/01-fundamentos-nodejs/streams/fundamentos.js
@@ -0,0 +1,11 @@
+// importação de clientes via CSV(excel)
+//1gb 
+// POST /upload import.csv
+
+//10mb/s 100s
+
+// / 100s -> Inserções no banco de dados
+//ler os dados do upload(HTTP) aos poucos e 
+// processando enquanto está sendo enviado
+
+//readable Streams / Writable Streams
