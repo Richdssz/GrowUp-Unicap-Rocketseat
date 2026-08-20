c1d24c3c1e5f64605189ce5d8b0fbb49ce207a28
Richard Silva <richarda50x@gmail.com>
2026-08-20 13:08:27 -0300

    Streams no Node.js - Criando stream de leitura

--- aulas-node/01-fundamentos-nodejs/streams/fundamentos.js
+++ aulas-node/01-fundamentos-nodejs/streams/fundamentos.js
@@ -1,11 +1,33 @@
 // importação de clientes via CSV(excel)
-//1gb 
+//1gb
 // POST /upload import.csv
 
 //10mb/s 100s
 
 // / 100s -> Inserções no banco de dados
-//ler os dados do upload(HTTP) aos poucos e 
+//ler os dados do upload(HTTP) aos poucos e
 // processando enquanto está sendo enviado
 
 //readable Streams / Writable Streams
+//stams do 0
+
+import { Readable } from "node:stream";
+
+class OneToHundredStream extends Readable {
+  index = 1;
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } 
+      else {
+        const buf = Buffer.from(String(i));
+        this.push(" " + buf);
+      }
+    }, 1000);
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout);
